f239bb0f53b0faed6058df12c153b444e0631f10
Sumama Rahim <sumamarahim@outlook.com>
2026-09-07 15:23:33 +0500

    Add merge function to merge two sorted arrays
    Implement merge function to combine two sorted arrays in-place.

---
+++ 7_88.py
@@ -0,0 +1,29 @@
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        
+
+        # intuition
+        # Approach one: using sort()
+        # j = m
+        # for i in range(n):
+        #     nums1[j] = nums2[i]
+        #     j += 1
+        # return nums1.sort()
+
+        # appraoch two: using two pointers technique
+
+        i = m - 1
+        j = n - 1
+        k = m + n - 1
+        while j >= 0:
+            if i >= 0 and nums1[i] > nums2[j]:
+                nums1[k] = nums1[i]
+                i -= 1
+            else:
+                nums1[k] = nums2[j]
+                j -= 1
+            k -= 1
+        return nums1
